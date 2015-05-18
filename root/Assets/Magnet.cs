ba17431294b16ad8f15ef79a6ec478bef46b265a
swagasaurusrex <i.primeaux@yahoo.com>
2015-05-18 14:49:49 -0500

    Game
    LVL expansion, Magnet

--- root/Assets/Magnet.cs
+++ root/Assets/Magnet.cs
@@ -1,15 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Magnet : MonoBehaviour {
+public class Magnet : MonoBehaviour
+{
+    [SerializeField]
+    private Transform destination;
 
-	// Use this for initialization
+    public float power = 100;
+    //public Vector3 teleportPoint;
+    public Rigidbody2D rb;
+    void Start()
+    {
+        //rb = GetComponent<Rigidbody2D>();
+    }
+    //void OnTriggerEnter2D()
+    //{
+      //  rb.MovePosition(transform.position + transform.right * Time.deltaTime);
+    //}
+
+    void OnTriggerStay2D()
+    {
+        rb.AddForce(Vector2.right * power);
+    }
+}
+
+
+
+	/*
     [SerializeField]
     private GameObject teleporter;
     [SerializeField]
     private Transform destination;
     [SerializeField]
     private GameObject player;
+
+
+
 
 	 private void teleport(GameObject player, Transform destination)
     {
@@ -25,3 +51,4 @@ public class Magnet : MonoBehaviour {
          teleport(player, destination);
      }
 }
+    */
