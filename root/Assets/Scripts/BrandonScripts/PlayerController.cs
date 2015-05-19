6e5632a9cb6e72eee2001aa4f3b8c9ed00dcecca
BrandonCoco <bmcoco@earthlink.net>
2015-05-19 16:59:52 -0500

    Player Controller Changes
    Added a new "test" player controller script. Seems to be working a lot
    better. Player now loops through walk animation and rotates according to
    the button pushed and has a lopped idle animation when nothing is
    pressed. Some things are still fucked up in the script though.

--- root/Assets/Scripts/BrandonScripts/PlayerController.cs
+++ root/Assets/Scripts/BrandonScripts/PlayerController.cs
@@ -10,7 +10,7 @@ public class PlayerController : MonoBehaviour
 	Rigidbody2D myBody;
 	[SerializeField]
 	private Animator _anim;
-	
+
 	bool isGrounded = false;
 
 	void Awake()
@@ -25,7 +25,6 @@ public class PlayerController : MonoBehaviour
 		myBody = GetComponent<Rigidbody2D>();
 		myTrans = this.transform;
 		tagGround = GameObject.Find (this.name + "/tag_ground").transform;
-		_anim.SetBool ("idle", true);
 	}
 
 	
@@ -37,21 +36,10 @@ public class PlayerController : MonoBehaviour
 		Move (Input.GetAxisRaw ("Horizontal"));
 		if (Input.GetButtonDown ("Jump"))
 		   	Jump ();
-		/*
-		 * when you press d face right
-		 * when you press a face left
-		 * when you are not pressing anything idle and face whatever it means to idle
-		 * switch(input)
-		 * case a: face right break;
-		 * case d: face left break;
-		 * default: idle face idle break;
-		 * just write code that turns him when you press input
-		 */
 
 		if (Input.GetKeyDown (KeyCode.D))
 		{
 			{
-				//_anim.SetBool("idle",false);
 				_anim.SetBool ("right", false);
 				this.gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 200, 0));
 			}
@@ -62,7 +50,6 @@ public class PlayerController : MonoBehaviour
 		if (Input.GetKeyUp (KeyCode.D)) 
 		{
 			{
-				//right = false;
 				_anim.SetBool ("idle", true);
 			}
 			Debug.Log ("IdleRight");
@@ -81,7 +68,6 @@ public class PlayerController : MonoBehaviour
 		if (Input.GetKeyUp (KeyCode.A)) 
 		{
 			{
-				//left = false;
 				_anim.SetBool ("idle", true);
 			}
 			Debug.Log("IdleLeft");
@@ -89,44 +75,21 @@ public class PlayerController : MonoBehaviour
 
 	}
 	
-	void Move(float horizonalInput)
+	void Move(float horizontalInput)
 	{
 		if(!canMoveInAir && !isGrounded)
 			return;
 		
 		Vector2 moveVel = myBody.velocity;
-		moveVel.x = horizonalInput * speed;
+		moveVel.x = horizontalInput * speed;
 		myBody.velocity = moveVel;
 	}
 
 	//set to false
 	public void Jump()
 	{
-		if(isGrounded)
+		if (isGrounded)
 			myBody.velocity += jumpVelocity * Vector2.up;
 	}
-
-	/*
-	void FixedUpdate ()
-	{
-		isGrounded = Physics2D.Linecast (myTrans.position, tagGround.position, playerMask);
-
-		Move(Input.GetAxisRaw("Horizontal"));
-		if(Input.GetButtonDown("Jump"))
-			Jump();
-	
-	}
-	
-	void Move(float horizonalInput)
-	{
-		if(!canMoveInAir && !isGrounded)
-		    return;
-
-		Vector2 moveVel = myBody.velocity;
-		moveVel.x = horizonalInput * speed;
-		myBody.velocity = moveVel;
-	}
-	*/
-
 
 }
--- root/Assets/Scripts/BrandonScripts/PlayerControllerTest.cs
+++ root/Assets/Scripts/BrandonScripts/PlayerControllerTest.cs
@@ -1,15 +1,79 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerControllerTest : MonoBehaviour {
+public class PlayerControllerTest : MonoBehaviour 
+{
+	public float speed = 10, jumpVelocity = 10;
+	public LayerMask playerMask;
+	public bool canMoveInAir = true;
+	Transform myTrans, tagGround;
+	Rigidbody2D myBody;
+	[SerializeField]
+	//private Animator _anim;
 
-	// Use this for initialization
-	void Start () {
-	
+	bool isGrounded = false;
+
+	Animator anim;
+	int jumpHash = Animator.StringToHash("Jump");
+	int runStateHash = Animator.StringToHash("Base Layer.Run");
+
+
+	void Awake()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	void Start ()
+	{
+		myBody = GetComponent<Rigidbody2D>();
+		myTrans = this.transform;
+		tagGround = GameObject.Find (this.name + "/tag_ground").transform;
+		//_anim.SetBool ("idle", true);
 	}
 	
-	// Update is called once per frame
-	void Update () {
 	
+	void FixedUpdate ()
+	{
+
+		isGrounded = Physics2D.Linecast (myTrans.position, tagGround.position, playerMask);
+
+		float move = Input.GetAxis ("Horizontal");
+		//move (Input.GetAxisRaw ("Horizontal"));
+		anim.SetFloat("Speed", move);
+
+		
+		AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+		if(Input.GetKeyDown(KeyCode.Space) && stateInfo.nameHash == runStateHash)
+		{
+			anim.SetTrigger (jumpHash);
+		}
+
+		if (Input.GetKeyDown (KeyCode.D)) 
+		{
+			
+			this.gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 90, 0));
+		}
+
+		if (Input.GetKeyDown (KeyCode.A)) 
+		{
+
+			this.gameObject.transform.rotation = Quaternion.Euler(new Vector3(0, 270, 0));
+		}
+	}
+	/*
+	void move(float horizontalInput)
+	{
+		if (!canMoveInAir && !isGrounded)
+			return;
+		
+		Vector2 moveVel = myBody.velocity;
+		moveVel.x = horizontalInput * speed;
+		myBody.velocity = moveVel;
+	}
+	*/
+	public void Jump()
+	{
+		if(isGrounded)
+			myBody.velocity += jumpVelocity * Vector2.up;
 	}
 }
