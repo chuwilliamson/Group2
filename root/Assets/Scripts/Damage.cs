22d7f00025317a829d7e412a31a9566f1fc1a077
swagasaurusrex <i.primeaux@yahoo.com>
2015-05-26 16:03:34 -0500

    More
    More Assets

---
+++ root/Assets/Scripts/Damage.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Damage : MonoBehaviour {
+
+	// Use this for initialization
+	 public int damage = 100;
+
+     void OnCollisionEnter2D(Collision2D col)
+     {
+
+         Player.GetComponent<PlayerStats>().currentHealth -= damage;
+     }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    [SerializeField]
+    GameObject Player;
+}
