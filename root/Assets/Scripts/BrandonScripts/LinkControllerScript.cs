8d8d4113a0022ae244eeee9bd7f9a98585d9b181
BrandonCoco <bmcoco@earthlink.net>
2015-06-08 15:24:53 -0500

    Base Changes
    Took off double jump on lumpy. Added camera prefab called LumpyCamera.
    Added lumpy and lumpcam to FinalGame scene

--- root/Assets/Scripts/BrandonScripts/LinkControllerScript.cs
+++ root/Assets/Scripts/BrandonScripts/LinkControllerScript.cs
@@ -9,6 +9,8 @@ public class LinkControllerScript : MonoBehaviour {
 	bool facingRight = true;
 	Rigidbody2D myBody;
 
+	Quaternion rotation;
+
 	Animator anim;
 
 	//used for ground
@@ -17,7 +19,8 @@ public class LinkControllerScript : MonoBehaviour {
 	float groundRadius = 0.2f;
 	public LayerMask WhatisGround;
 	public float jumpForce = 700;
-	public bool doubleJump = false;
+
+	//public bool doubleJump = false;
 
 	//
 	// Use this for initialization
@@ -26,15 +29,30 @@ public class LinkControllerScript : MonoBehaviour {
 		myBody = GetComponent<Rigidbody2D>();
 	
 	}
-	
+
+	//Stop camera rotation, but also stops character from rotating
+	/*
+	void Awake()
+	{
+		rotation = transform.rotation;
+	}
+
+	void LateUpdate()
+	{
+		transform.rotation = rotation;
+	}
+	*/
+
 	// FixedUpdate is called once per fixed frame
 	void FixedUpdate () {
 
 		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, WhatisGround);
 		anim.SetBool ("Ground", grounded);
 
+		/* //Uncomment this for double jump
 		if(grounded)
 			doubleJump = false;
+		*/
 
 		anim.SetFloat ("vSpeed", myBody.velocity.y);
 
@@ -44,31 +62,52 @@ public class LinkControllerScript : MonoBehaviour {
 
 		float move = Input.GetAxis ("Horizontal");
 
+
+		if (Input.GetKeyDown (KeyCode.A)) {
+			{
+				this.gameObject.transform.rotation = Quaternion.Euler (new Vector3 (0, 270, 0));
+			}
+		}
+
+		if (Input.GetKeyDown (KeyCode.D)) {
+			{
+				this.gameObject.transform.rotation = Quaternion.Euler (new Vector3 (0, 90, 0));
+			}
+		}
+
 		anim.SetFloat ("Speed", Mathf.Abs (move));
 
 		myBody.velocity = new Vector2 (move * maxSpeed, myBody.velocity.y);
 
+		/*
 		// Flips the camera
 		if (move > 0 && !facingRight)
 			Flip ();
 		else if (move < 0 && facingRight)
 			Flip ();
-	
+	*/
 	}
+
 
 	void Update()
 	{
-		if ((grounded || !doubleJump) && Input.GetKeyDown (KeyCode.Space)) {
+			//double jump coding
+		//if ((grounded || !doubleJump) && Input.GetKeyDown (KeyCode.Space))
+
+		if (grounded && Input.GetKeyDown (KeyCode.Space)) 
+		{
 			anim.SetBool ("Ground", false);
 			myBody.AddForce (new Vector2 (0, jumpForce));
 
+			/* //double jumping stuff
 			if(!doubleJump && !grounded)
 				doubleJump = true;
-			
-			} 
+			*/
+		} 
 	}
 
 	//flip camera function
+	/*
 	void Flip()
 	{
 		facingRight = !facingRight;
@@ -76,5 +115,6 @@ public class LinkControllerScript : MonoBehaviour {
 		theScale.x *= -1;
 		transform.localScale = theScale;
 	}
+	*/
 }
 
