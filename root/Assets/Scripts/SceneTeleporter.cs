0807245cd4b528916a13db0913e42952443b446b
BrandonCoco <bmcoco@earthlink.net>
2015-06-10 12:56:33 -0500

    New Stages and Scripts
    Added NextScene script for obviously teleporting to the next scene.
    Added folder called "FinalGameScenes" we need to put all level we want
    in the game in this folder. Added lvl1 to this folder and started making
    it.

--- root/Assets/Scripts/SceneTeleporter.cs
+++ root/Assets/Scripts/SceneTeleporter.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class SceneTeleporter : MonoBehaviour {
 
+	[SerializeField]
+	private GameObject teleporter;
+	[SerializeField]
+	private GameObject player;
+	public string Level;
+
 	// Use this for initialization
-	void Start () {
-	
+	void OnTriggerEnter2D()
+	{
+		Application.LoadLevel(Level);
 	}
-	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update()
+	{
+
 	}
+
+	/*
+	function OnTriggerEnter (other: Collider){
+		Application.LoadLevel( 2);
+	}
+	*/
 }
